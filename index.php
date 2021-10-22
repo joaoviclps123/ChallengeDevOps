3ba029a0b942214f65730ce7ed0e9226d82ad01b
João Victor <44348486+joaoviclps123@users.noreply.github.com>
2021-10-22 09:11:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <main class="container p-4">
   <div class="row">
     <div class="col-md-4">
-      <!-- MESSAGES -->
+     
 
       <?php if (isset($_SESSION['message'])) { ?>
       <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
